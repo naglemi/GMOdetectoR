bb9e73fc082848f8f886371e858c9a708f5d9fcf
Michael Nagle <naglemi@steed.eecs.oregonstate.edu>
2019-10-31 14:27:46 -0700

    Added PhenotypeAssistant.R code to the project

---
+++ PhenotypeAssistantv0.01.R
@@ -0,0 +1,70 @@
+# PhenotypeAssistant.R
+# Requires GMOdetectoR backend, originally version 0.18
+
+source("/scratch2/NSF_GWAS/GMOdetectoR/GMOdetectoRv0.18.R")
+
+chroma_standard_path <- "/scratch2/NSF_GWAS/macroPhor_Array/CT_CU_CV_raw/wk6/chroma_F2.2_I0.85_L100_cyan_005537_0_0_0.raw"
+
+chroma_in <- load_image(image_path = chroma_standard_path)
+
+phenotype_assistant <- function(image_path, chroma_standard_preloaded, grid_type=20){
+  img_in_backend <<- load_image(image_path = image_path)
+  
+  reduced_img_in_backend <- reduce_image(img_in_backend)
+  
+  plotting_mode <- "hyperspectral"
+  
+  image_spectrum_table_colored <- extract_plot_grid_item(image_spectrum_table = reduced_img_in_backend,
+                                                         chroma_table = chroma_table,
+                                                         FC = TRUE,
+                                                         cap = TRUE,
+                                                         max_intensity_FP = 800,
+                                                         max_intensity_Chl = 800,
+                                                         scale = TRUE,
+                                                         normalize = FALSE,
+                                                         to_denoise = TRUE,
+                                                         standardize_rgb = TRUE,
+                                                         denoise_threshold_FP = 400,
+                                                         denoise_threshold_Chl = 400,
+                                                         dim_chlorophyll = as.numeric(1),
+                                                         FP = input$FP)
+  
+  
+  what_to_plot <- decide_what_to_plot(mode = c(1))
+  
+  # First let's print the whole plate
+  whole_plate_plot <- crop_and_plot(mode=what_to_plot,
+                                    input_toggle=TRUE,
+                                    # The below line is currently the same for any type of plot
+                                    image_spectrum_table = image_spectrum_table_colored,
+                                    grid_item = 18,
+                                    image_type = "hyperspectral",
+                                    first_pass_FP_threshold = 200,
+                                    first_pass_Chl_threshold = 200)
+  
+  print(whole_plate_plot)
+  
+  # Now each individual explant
+  if(grid_type!=20){
+    stop("Support not added yet for grid types other than 20 explant")
+  }
+  
+  if(grid_type=20){
+    for(i in 1:20){
+      this_explant_plot <- crop_and_plot(mode=what_to_plot,
+                                         input_toggle=TRUE,
+                                         # The below line is currently the same for any type of plot
+                                         image_spectrum_table = image_spectrum_table_colored,
+                                         grid_item = 18,
+                                         image_type = "hyperspectral",
+                                         first_pass_FP_threshold = 200,
+                                         first_pass_Chl_threshold = 200)
+      print(this_explant_plot)
+    }
+  }
+  
+}
+
+phenotype_assistant(image_path = paste0("/scratch2/NSF_GWAS/macroPhor_Array/CT_CU_CV_raw/wk6/",
+                                        filename),
+                    chroma_standard_preloaded = chroma_in)
