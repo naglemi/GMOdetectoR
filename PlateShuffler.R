d29ae985c62ec3343ab3bd1c62e147184b77eb54
Michael Nagle <naglemi@steed.eecs.oregonstate.edu>
2019-11-08 15:25:25 -0800

    Got PlateShuffler to give us 200 randomly selected images for manual scoring

--- PlateShuffler.R
+++ PlateShuffler.R
@@ -1,8 +1,15 @@
 # Load GMOdetectoR backend, needed for: 1) assign_tray_plate_ID, 2)get_row_col_for_grid_item, ...
 
-source("C:/Users/OSU/code/GMOdetectoR/GMOdetectoRv0.19.R")
+library(stringr)
 
-source_directory <- "Z:/Groups/Tgerc/RESEARCH PROJECTS/NSF project/In Vitro GWAS study/Randomization_Datasheets"
+source("/scratch2/NSF_GWAS/GMOdetectoR/GMOdetectoRv0.19.R")
+#source("C:/Users/OSU/code/GMOdetectoR/GMOdetectoRv0.19.R")
+
+# Need exifr in order to read exif data from images and keep only the most recent one of each
+#install.packages("exifr")
+library(exifr)
+
+source_directory <- "/scratch2/NSF_GWAS/macroPhor_Array/Randomization_Datasheets"
 source_files <- list.files(source_directory, full.names = TRUE)
 
 randomization_database <- data.table(matrix(ncol = ncol(fread(source_files[1]))))
@@ -16,7 +23,31 @@ for(file in source_files){
   randomization_database <- rbind(randomization_database, this_file)
 }
 
-all_images_directory <- "J:/Downloads/copy_all_jpgs_on_Box_as_of_11.5.19_sync/"
-image_files <- list.files(all_images_directory, full.names=TRUE)
+all_images_directory <- "/scratch2/NSF_GWAS/macroPhor_Array/all_jpgs_as_of_11.6.19/"
+image_files <- list.files(all_images_directory, full.names=TRUE, pattern="", recursive = TRUE)
+
+image_metadata <- read_exif(image_files)
+image_creation_dates <- image_metadata$CreateDate
+image_db <- data.table(cbind(image_files, image_creation_dates))
+
+image_db$timepoint <- str_split_fixed(image_files, "/", 9)[,8]
+
+image_basename <- basename(file_path_sans_ext(image_files))
+
+image_db$TrayID <- str_split_fixed(image_basename, "_", 2)[,1]
+
+image_db$row_col <- paste0(str_split_fixed(image_basename, "_", 9)[,7],
+                           "_",
+                           str_split_fixed(image_basename, "_", 9)[,8])
+
+image_basename <- gsub("_rgb", "", image_basename)
+
+image_db$PlateID <- rep(NA, nrow(image_db))
+for(i in length(image_basename)){
+  this_ID <- assign_ID_index_from_row_column_on_tray(data_to_parse = image_basename[i], mode="filename")
+  image_db$PlateID[i] <- this_ID
+}
+
+
 
 image_files <- basename(tools::file_path_sans_ext(image_files))
